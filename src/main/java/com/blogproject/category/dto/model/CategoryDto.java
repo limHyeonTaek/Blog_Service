22f36cb9850bc4af81f2e3e836351ca55e9c7552
Hyeontaek <lht990e@naver.com>
2024-01-21 19:15:28 +0900

    feat:category 생성 기능

---
+++ src/main/java/com/blogproject/category/dto/model/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.blogProject.category.dto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+  private String categoryName;
+
+}
