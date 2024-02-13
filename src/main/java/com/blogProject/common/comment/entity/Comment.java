ef4856828e5e9cab132df4c1d583f1681adcc1c8
Hyeontaek <lht990e@naver.com>
2024-02-13 14:45:20 +0900

    feat:대댓글 작성 기능 구현

--- src/main/java/com/blogProject/common/comment/entity/Comment.java
+++ src/main/java/com/blogProject/common/comment/entity/Comment.java
@@ -3,12 +3,17 @@ package com.blogProject.common.comment.entity;
 import com.blogProject.common.entity.BaseTimeEntity;
 import com.blogProject.common.member.entity.Member;
 import com.blogProject.common.post.entity.Post;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,5 +40,17 @@ public class Comment extends BaseTimeEntity {
 
   @ManyToOne
   private Post post;
+
+  @ManyToOne
+  @JoinColumn(name = "parent_id")
+  private Comment parentComment;
+
+  @Builder.Default
+  @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL)
+  private List<Comment> childComments = new ArrayList<>();
+
+  public boolean isWrittenBy(Member member) {
+    return this.member.equals(member);
+  }
 
 }
--- src/main/java/com/blogProject/common/comment/service/CommentService.java
+++ src/main/java/com/blogProject/common/comment/service/CommentService.java
@@ -5,7 +5,9 @@ import static com.blogProject.exception.ErrorCode.COMMENT_NOT_FOUND;
 import static com.blogProject.exception.ErrorCode.MEMBER_NOT_FOUND;
 import static com.blogProject.exception.ErrorCode.POST_NOT_FOUND;
 
+import com.blogProject.common.comment.dto.WriteReply;
 import com.blogProject.common.comment.dto.model.CommentDto;
+import com.blogProject.common.comment.dto.model.ReplyDto;
 import com.blogProject.common.comment.entity.Comment;
 import com.blogProject.common.comment.exception.CommentException;
 import com.blogProject.common.comment.repository.CommentRepository;
@@ -31,58 +33,80 @@ public class CommentService {
   private final MemberRepository memberRepository;
   private final PostRepository postRepository;
 
-  // 댓글 작성
+  // 댓글 생성
   @Transactional
   public CommentDto writeComment(Long postId, String request, Authentication authentication) {
-    Member member = memberRepository.findByEmail(authentication.getName())
-        .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
-    Post post = postRepository.findById(postId)
-        .orElseThrow(() -> new PostException(POST_NOT_FOUND));
+    Member member = getMemberByEmail(authentication.getName());
+    Post post = getPostById(postId);
     Comment comment = Comment.builder()
         .post(post)
         .member(member)
         .comments(request)
         .build();
-    CommentDto commentDto = CommentDto.fromEntity(comment);
     commentRepository.save(comment);
-    return commentDto;
+    return CommentDto.fromEntity(comment);
   }
 
-  // 댓글 수정
+  // 댓글 수정(대댓글 포함)
   @Transactional
   public CommentDto updateComment(Long commentId, String request, Authentication authentication) {
-    Comment comment = findCommentAndMatchMember(commentId,
-        authentication);
-
+    Comment comment = findCommentAndMatchMember(commentId, authentication);
     comment.setComments(request);
     return CommentDto.fromEntity(comment);
   }
 
-  // 댓글 삭제
+  // 댓글 삭제(대댓글 포함)
   @Transactional
   public void deleteComment(Long commentId, Authentication authentication) {
     Comment comment = findCommentAndMatchMember(commentId, authentication);
-
     commentRepository.delete(comment);
   }
 
-  // 댓글 보기(페이징 처리)
+  // 댓글 조회(대댓글 포함)
+  @Transactional
   public Page<CommentDto> getComments(Long postId, Pageable pageable) {
-    Post post = postRepository.findById(postId)
-        .orElseThrow(() -> new PostException(POST_NOT_FOUND));
+    Post post = getPostById(postId);
     Page<Comment> comments = commentRepository.findByPost(post, pageable);
     return comments.map(CommentDto::fromEntity);
   }
 
+  // 대댓글 생성
+  @Transactional
+  public ReplyDto writeReply(Long commentId, WriteReply request, Authentication authentication) {
+    Member member = getMemberByEmail(authentication.getName());
+    Comment parentComment = getCommentById(commentId);
+    Comment childComment = Comment.builder()
+        .member(member)
+        .post(parentComment.getPost())
+        .comments(request.getReply())
+        .parentComment(parentComment)
+        .build();
+    commentRepository.save(childComment);
+    return ReplyDto.fromEntity(childComment);
+  }
+
+  private Member getMemberByEmail(String email) {
+    return memberRepository.findByEmail(email)
+        .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND));
+  }
+
+  private Post getPostById(Long postId) {
+    return postRepository.findById(postId)
+        .orElseThrow(() -> new PostException(POST_NOT_FOUND));
+  }
+
+  private Comment getCommentById(Long commentId) {
+    return commentRepository.findById(commentId)
+        .orElseThrow(() -> new CommentException(COMMENT_NOT_FOUND));
+  }
 
   private Comment findCommentAndMatchMember(Long commentId, Authentication authentication) {
-    Comment comment = commentRepository.findById(commentId)
-        .orElseThrow(() -> new CommentException(COMMENT_NOT_FOUND));
-
-    if (!comment.getMember().getEmail().equals(authentication.getName())) {
+    Comment comment = getCommentById(commentId);
+    Member member = getMemberByEmail(authentication.getName());
+    if (!comment.isWrittenBy(member)) {
       throw new GlobalException(ACCESS_DENIED);
     }
     return comment;
   }
-
 }
+
